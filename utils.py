29ecfe5a5e3f8963a0acc1deac9d1221f16ca88f
AdamKacprzycki <adamkacprzycki@gmail.com>
2023-06-07 11:09:17 +0200

    optimize func have been changed

--- utils.py
+++ utils.py
@@ -45,49 +45,136 @@ def optimize_ints(df: pd.DataFrame) -> pd.DataFrame:
     return df
 
 
-def optimize_objects(df: pd.DataFrame, datetime_features: List[str]) -> pd.DataFrame:
+# def optimize_objects(df: pd.DataFrame, datetime_features: List[str]) -> pd.DataFrame:
     
+#     '''
+#     The function optimize_objects takes a pandas DataFrame as input, along with a list of column names (datetime_features) that are supposed to be 
+#     of datetime type. The function optimizes the memory usage of object columns in the DataFrame.
+
+#     It first loops over all object columns in the DataFrame using select_dtypes method with include=['object']. For each column, if it is not in 
+#     datetime_features and not of type list, the function checks if the ratio of the number of unique values to the total number of values in the 
+#     column is less than 0.5. If it is, the column is converted to the 'category' data type, which can save memory compared to object data type.
+
+#     If a column is in datetime_features, it is converted to datetime type using pd.to_datetime method. Finally, the function returns the modified DataFrame.
+#     '''
+    
+    
+#     for col in df.select_dtypes(include=['object']):
+#         if col not in datetime_features:
+#             if not (type(df[col][0])==list):
+#                 num_unique_values = len(df[col].unique())
+#                 num_total_values = len(df[col])
+#                 if float(num_unique_values) / num_total_values < 0.5:
+#                     df[col] = df[col].astype('category')
+#         else:
+#             df[col] = pd.to_datetime(df[col])
+#     return df
+
+# def optimize_objects(df: pd.DataFrame, datetime_features: List[str], bool_features: List[str]) -> pd.DataFrame:
+#     '''
+#     The function optimize_objects takes a pandas DataFrame as input, along with a list of column names (datetime_features) that are supposed to be 
+#     of datetime type, and a list of column names (bool_features) that are supposed to be of bool type. The function optimizes the memory usage of 
+#     object columns in the DataFrame.
+
+#     It first loops over all object columns in the DataFrame using select_dtypes method with include=['object']. For each column, if it is not in 
+#     datetime_features or bool_features and not of type list, the function checks if the ratio of the number of unique values to the total number 
+#     of values in the column is less than 0.5. If it is, the column is converted to the 'category' data type, which can save memory compared to 
+#     object data type.
+
+#     If a column is in datetime_features, it is converted to datetime type using pd.to_datetime method.
+    
+#     If a column is in bool_features, it is converted to bool type using astype(bool) method.
+    
+#     Finally, the function returns the modified DataFrame.
+#     '''
+    
+#     for col in df.select_dtypes(include=['object']):
+#         if col not in datetime_features and col not in bool_features:
+#             if not (type(df[col][0]) == list):
+#                 num_unique_values = len(df[col].unique())
+#                 num_total_values = len(df[col])
+#                 if float(num_unique_values) / num_total_values < 0.5:
+#                     df[col] = df[col].astype('category')
+#         elif col in datetime_features:
+#             df[col] = pd.to_datetime(df[col])
+#         elif col in bool_features:
+#             df[col] = df[col].astype(bool)
+    
+#     return df
+
+
+
+# def optimize(df: pd.DataFrame, datetime_features: List[str] = [], bool_features: List[str]):
+    
+#     '''
+#     The function optimize takes a pandas DataFrame as input, along with an optional list of column names (datetime_features) that are supposed to be of
+#     datetime type. The function optimizes the memory usage of integer, float, and object columns in the DataFrame using the optimize_ints, optimize_floats,
+#     and optimize_objects functions.
+
+#     The function first calculates the memory usage of the input DataFrame using sys.getsizeof method. It then calls the three optimization functions in the
+#     following order: optimize_objects, optimize_ints, and optimize_floats.
+
+#     After the optimization is complete, the function prints out the percentage of memory reduction achieved by the optimization. 
+#     Finally, the function does not return anything, neverthelss it change the types of passed DataFrame.
+#     '''
+    
+#     mem_usage_bef = sys.getsizeof(df)
+#     optimize_floats(optimize_ints(optimize_objects(df, datetime_features, bool_features)))
+#     print(f'Optimize_memory_func reduce memory usage by {(1-(sys.getsizeof(df)/mem_usage_bef))*100 :.2f} %.')
+
+def optimize_objects(df: pd.DataFrame, datetime_features: List[str], bool_features: List[str]) -> pd.DataFrame:
     '''
     The function optimize_objects takes a pandas DataFrame as input, along with a list of column names (datetime_features) that are supposed to be 
-    of datetime type. The function optimizes the memory usage of object columns in the DataFrame.
+    of datetime type, and a list of column names (bool_features) that are supposed to be of bool type. The function optimizes the memory usage of 
+    object columns in the DataFrame.
 
     It first loops over all object columns in the DataFrame using select_dtypes method with include=['object']. For each column, if it is not in 
-    datetime_features and not of type list, the function checks if the ratio of the number of unique values to the total number of values in the 
-    column is less than 0.5. If it is, the column is converted to the 'category' data type, which can save memory compared to object data type.
+    datetime_features or bool_features and not of type list, the function checks if the ratio of the number of unique values to the total number 
+    of values in the column is less than 0.5. If it is, the column is converted to the 'category' data type, which can save memory compared to 
+    object data type.
 
-    If a column is in datetime_features, it is converted to datetime type using pd.to_datetime method. Finally, the function returns the modified DataFrame.
+    If a column is in datetime_features, it is converted to datetime type using pd.to_datetime method.
+    
+    If a column is in bool_features, it is converted to bool type using astype(bool) method.
+    
+    Finally, the function returns the modified DataFrame.
     '''
     
-    
     for col in df.select_dtypes(include=['object']):
-        if col not in datetime_features:
-            if not (type(df[col][0])==list):
+        if col not in datetime_features and col not in bool_features:
+            if not (type(df[col][0]) == list):
                 num_unique_values = len(df[col].unique())
                 num_total_values = len(df[col])
                 if float(num_unique_values) / num_total_values < 0.5:
                     df[col] = df[col].astype('category')
-        else:
+        elif col in datetime_features:
             df[col] = pd.to_datetime(df[col])
+    
+    for col in bool_features:
+        if col in df.columns:
+            df[col] = df[col].astype(bool)
+    
     return df
 
 
-def optimize(df: pd.DataFrame, datetime_features: List[str] = []):
-    
+def optimize(df: pd.DataFrame, datetime_features: List[str] = [], bool_features: List[str] = []):
     '''
     The function optimize takes a pandas DataFrame as input, along with an optional list of column names (datetime_features) that are supposed to be of
-    datetime type. The function optimizes the memory usage of integer, float, and object columns in the DataFrame using the optimize_ints, optimize_floats,
-    and optimize_objects functions.
+    datetime type, and an optional list of column names (bool_features) that are supposed to be of bool type. The function optimizes the memory usage of 
+    integer, float, and object columns in the DataFrame using the optimize_objects, optimize_ints, and optimize_floats functions.
 
     The function first calculates the memory usage of the input DataFrame using sys.getsizeof method. It then calls the three optimization functions in the
     following order: optimize_objects, optimize_ints, and optimize_floats.
 
     After the optimization is complete, the function prints out the percentage of memory reduction achieved by the optimization. 
-    Finally, the function does not return anything, neverthelss it change the types of passed DataFrame.
+    Finally, the function does not return anything, nevertheless, it changes the types of the passed DataFrame.
     '''
     
     mem_usage_bef = sys.getsizeof(df)
-    optimize_floats(optimize_ints(optimize_objects(df, datetime_features)))
-    print(f'Optimize_memory_func reduce memory usage by {(1-(sys.getsizeof(df)/mem_usage_bef))*100 :.2f} %.')
+    df = optimize_objects(df, datetime_features, bool_features)
+    optimize_ints(df)
+    optimize_floats(df)
+    print(f'Optimize_memory_func reduce memory usage by {(1 - (sys.getsizeof(df) / mem_usage_bef)) * 100 :.2f} %.')
     
 ###############################################-OUTLIER-DETECTION-BOXPLOTS-###############################################
     
